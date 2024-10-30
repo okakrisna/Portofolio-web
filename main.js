3ffb8f2def48f96402ded9d355688be03d185e42
I Made Oka Krisna Putra <48183294+okakrisna@users.noreply.github.com>
2024-10-30 12:12:28 +0800

    web-porto

---
+++ main.js
@@ -0,0 +1,29 @@
+function scrollImg() {
+  const slideDown = document.getElementById("imgDown");
+  slideDown.classList.toggle("active");
+}
+
+function scrollImgOut() {
+  const slideDown = document.getElementById("imgDown");
+  slideDown.classList.toggle("active");
+}
+
+function scrollImg2() {
+  const slideDown2 = document.getElementById("imgDown2");
+  slideDown2.classList.toggle("active");
+}
+
+function scrollImgOut2() {
+  const slideDown2 = document.getElementById("imgDown2");
+  slideDown2.classList.toggle("active");
+}
+
+function scrollImg3() {
+  const slideDown3 = document.getElementById("imgDown3");
+  slideDown3.classList.toggle("active");
+}
+
+function scrollImgOut3() {
+  const slideDown3 = document.getElementById("imgDown3");
+  slideDown3.classList.toggle("active");
+}
